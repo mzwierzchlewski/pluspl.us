dd91ae1386895434051c714064b62dd6c371fe1b
Maciej Zwierzchlewski <maciej.zwierzchlewski@cognifide.com>
2020-03-26 15:58:00 +0100

    Fixed chujo/turbo

--- plusplus/operations/leaderboard.py
+++ plusplus/operations/leaderboard.py
@@ -12,7 +12,7 @@ def generate_leaderboard(team=None, losers=False, global_leaderboard=False, snap
     
     if snapshot:
         ordering = Thing.points.desc()
-        header = "Last week standings:"
+        header = "Last month standings:"
 
     # filter args
     user_args = {"user": True}
@@ -46,33 +46,55 @@ def generate_leaderboard(team=None, losers=False, global_leaderboard=False, snap
                 ]
         }
 
-    filtered_users = sorted(list(filter(lambda u: u.points - u.last_week_points != 0, users)), key=lambda u: u.points - u.last_week_points)
+    sorted_users = sorted(users, key=lambda u: 0 - (u.points - u.last_week_points))
 
     if not global_leaderboard:
-        formatted_users = [f"<@{user.item.upper()}> ({user.points}) {':turbokotlarz:' if user.turbo else ''} {':rak2:' if user.chujo else ''}" for user in users] if not snapshot else [f"<@{user.item.upper()}> ({user.points - user.last_week_points})" for user in filtered_users]
+        formatted_users = [f"<@{user.item.upper()}> ({user.points}) {':turbokotlarz:' if user.turbo else ''} {':rak2:' if user.chujo else ''}" for user in users] if not snapshot else [f"<@{user.item.upper()}> ({user.points - user.last_week_points})" for user in sorted_users]
         numbered_users = generate_numbered_list(formatted_users)
         body['fields'].append({
                                   "type": "mrkdwn",
                                   "text": "*Users*\n" + numbered_users
                               })
-        if (snapshot and len(filtered_users) > 0):
+        if (snapshot and len(sorted_users) > 0):
+            turbo_users = list(filter(lambda u: u.points - u.last_week_points > 0, sorted_users))
             divider = {
 			    "type": "divider"
 		    }
             turbo = {
-                "type": "section",
-                "text": {
-                    "type": "mrkdwn",
-                    "text": ":turbokotlarz: :turbokotlarz: :turbokotlarz: *Turbokotlarz* :turbokotlarz: :turbokotlarz: :turbokotlarz:\n" + f"<@{filtered_users[0].item.upper()}>"
+                    "type": "section",
+                    "text": {
+                        "type": "mrkdwn",
+                        "text": ":turbokotlarz: :turbokotlarz: :turbokotlarz: *Turbokotlarz* :turbokotlarz: :turbokotlarz: :turbokotlarz:\n" + ":sadparrot:"
+                    }
                 }
-            }
+            if len(turbo_users) > 0:
+                turbo = {
+                    "type": "section",
+                    "text": {
+                        "type": "mrkdwn",
+                        "text": ":turbokotlarz: :turbokotlarz: :turbokotlarz: *Turbokotlarz* :turbokotlarz: :turbokotlarz: :turbokotlarz:\n" + f"<@{turbo_users[0].item.upper()}>"
+                    }
+                }
+            chujo_users = list(filter(lambda u: u.points - u.last_week_points <= 0, sorted_users))
             chujo = {
                 "type": "section",
                 "text": {
                     "type": "mrkdwn",
-                    "text": ":tn: :rak2: :zjebparrot: *Chujokotlarz* :zjebparrot: :rak2: :tn:\n" + f"<@{filtered_users[-1].item.upper()}>"
+                    "text": ":tn: :rak2: :zjebparrot: *Chujokotlarz* :zjebparrot: :rak2: :tn:\n" + ":alezapierdalaparrot:"
                 }
             }
+            if len(chujo_users) > 0:
+                chujo_list = ""
+                for user in chujo_users:
+                    chujo_list += f"<@{user.item.upper()}>\n"
+                chujo = {
+                    "type": "section",
+                    "text": {
+                        "type": "mrkdwn",
+                        "text": ":tn: :rak2: :zjebparrot: *Chujokotlarz* :zjebparrot: :rak2: :tn:\n" + chujo_list
+                    }
+                }
+            
             leaderboard = [leaderboard_header, body, divider, turbo, divider, chujo]
             return json.dumps(leaderboard)
 
